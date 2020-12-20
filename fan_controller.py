239fc6275c980ad8b9618d18ebc69bf6039aaf39
Davide Magnanimi <davidemagnanimi@gmail.com>
2020-12-20 10:54:57 +0100

    Improved logic

--- fan_controller.py
+++ fan_controller.py
@@ -2,14 +2,20 @@ import os
 from time import sleep
 import RPi.GPIO as GPIO
 
-FAN_CONTROL_PIN = 14    # GPIO14
-THRESHOLD = 60          # 60°C
-FAN_STATUS = True       # True = Active
+FAN_CONTROL_PIN: int = 14       # GPIO14
+THRESHOLD: float = 60           # 60°C
+FAN_STATUS: bool = False        # False = Not Active
 
 def setup() -> None:
+    global FAN_STATUS
+
+    GPIO.setwarnings(False)
+
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(FAN_CONTROL_PIN, GPIO.OUT)
-    GPIO.setwarnings(False)
+
+    # Set default status to off
+    GPIO.output(FAN_CONTROL_PIN, False)
 
 
 def set_status(new_status: bool = True):
@@ -28,16 +34,22 @@ def deactivate_fan() -> None:
 
 
 def evalute_temperature(cpu_temp: float, hysteresis: float = 10.0) -> None:
-    # If the CPU temperature is higher than the threshold we need to activate the fan
-    if cpu_temp >= THRESHOLD:
-        activate_fan()
-    
-    # We shutdown the fan only with a margin on the threshold
-    elif cpu_temp < (THRESHOLD - hysteresis):
-        deactivate_fan()
+    if FAN_STATUS:
+        if cpu_temp >= (THRESHOLD - hysteresis):
+            print(f'Current CPU temperature: {cpu_temp}°C. The fan is still active.')
+
+        # We shutdown the fan only with a margin on the threshold
+        else:
+            print(f'Current CPU temperature: {cpu_temp}°C. Deactivating the fan.')
+            deactivate_fan()
     
     else:
-        return
+        # If the CPU temperature is higher than the threshold we need to activate the fan
+        if cpu_temp >= THRESHOLD:
+            print(f'Current CPU temperature: {cpu_temp}°C. Activating the fan.')
+            activate_fan()
+        else:
+            print(f'Current CPU temperature: {cpu_temp}°C. No need to activate the fan.')
 
 
 def read_temperature() -> float: 
@@ -50,9 +62,7 @@ def main() -> None:
     setup()
     while True:
         temperature: int = read_temperature()
-
         evalute_temperature(cpu_temp=temperature)
-
         sleep(30)
 
 
